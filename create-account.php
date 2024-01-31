3b2b09846976289a55ab853aa0a5ddcd33e47c7b
Antoine <35777934+Charpentemars@users.noreply.github.com>
2024-01-31 16:32:43 +0100

    Add a password hash for DB

--- create-account.php
+++ create-account.php
@@ -1,7 +1,8 @@
 <?php
 // Connexion à MySQL
 $bdd = new PDO('mysql:host=localhost;dbname=20240124-creation-compte;charset=utf8', 'root', '');
+$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
 // Insertion du message à l'aide d'une requête préparée
 $req = $bdd->prepare('INSERT INTO users (email, password) VALUES(?, ?)');
-$req->execute(array($_POST['email'], $_POST['password']));
+$req->execute(array($_POST['email'], $password));
